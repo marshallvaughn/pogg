ae3c1b35b142792fb5c2dce61b36420102d61f11
Marshall Vaughn <marshall@hypecraft.io>
2016-10-19 21:25:55 -0400

    Changed checkmark to 'x'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var clc = require('cli-color'); // .................................... i use cli-color, it's the only one i've tried that properly shows color in papertrail
-var stringify = require('json-stringify-safe'); // .................... to simplify json object logging
-var figlet = require('figlet');
+var clc       = require('cli-color'); // .................................... i use cli-color, it's the only one i've tried that properly shows color in papertrail
+var stringify = require('json-stringify-safe'); // .......................... to simplify json object logging
+var figlet    = require('figlet');
 
 function gigantize(msg, clr) {
   if (typeof msg === 'object') {
@@ -28,11 +28,11 @@ function gigantize(msg, clr) {
 }
 
 var color = {
-  red: msg => clc.red(extract(msg)),
-  blue: msg => clc.blue(extract(msg)),
-  green: msg => clc.green(extract(msg)),
-  yellow: msg => clc.yellow(extract(msg)),
-  magenta: msg => clc.magenta(extract(msg))
+  red     : msg => clc.red( extract(msg) ),
+  blue    : msg => clc.blue( extract(msg) ),
+  green   : msg => clc.green( extract(msg) ),
+  yellow  : msg => clc.yellow( extract(msg) ),
+  magenta : msg => clc.magenta( extract(msg) )
 }
 
 var logg = function (msg) {
@@ -40,21 +40,21 @@ var logg = function (msg) {
 }
 
 // colorized logging
-logg.red = msg => lg(color.red(msg)); // ............. red
-logg.blue = msg => lg(color.blue(msg)); // ........... blue
-logg.green = msg => lg(color.green(msg)); // ......... green
-logg.yellow = msg => lg(color.yellow(msg)); // ....... yellow
-logg.magenta = msg => lg(color.magenta(msg)); // ..... purple
+logg.red     = msg => lg( color.red(msg) ); // ............. red
+logg.blue    = msg => lg( color.blue(msg) ); // ........... blue
+logg.green   = msg => lg( color.green(msg) ); // ......... green
+logg.yellow  = msg => lg( color.yellow(msg) ); // ....... yellow
+logg.magenta = msg => lg( color.magenta(msg) ); // ..... purple
 
 // functional logging
-logg.doing = msg => logg(color.yellow('[ ] ' + extract(msg))); // to show that something is going to be done.
-logg.done = msg => logg(color.green('[✓] ' + extract(msg))); // . to show that something is now done.
+logg.doing = msg => logg( color.yellow('[ ] ' + extract(msg) ) ); // to show that something is going to be done.
+logg.done  = msg => logg( color.green('[x] ' + extract(msg) ) ); // . to show that something is now done.
 
-logg.big = msg => gigantize(msg);
-logg.big.red = msg => gigantize(msg, 'red');
-logg.big.blue = msg => gigantize(msg, 'blue');
-logg.big.green = msg => gigantize(msg, 'green');
-logg.big.yellow = msg => gigantize(msg, 'yellow');
+logg.big         = msg => gigantize(msg);
+logg.big.red     = msg => gigantize(msg, 'red');
+logg.big.blue    = msg => gigantize(msg, 'blue');
+logg.big.green   = msg => gigantize(msg, 'green');
+logg.big.yellow  = msg => gigantize(msg, 'yellow');
 logg.big.magenta = msg => gigantize(msg, 'magenta');
 
 // globalize things if you wanna get really brazen. don't tell crockford.
